d603ea45c193cdc0158fa6e0a0104c9688f34084
Mr.76 <bartmoss77@tutanota.com>
2023-01-02 16:17:28 -0300

    changing time

--- bat.lua
+++ bat.lua
@@ -1,5 +1,5 @@
 local timer = vim.loop.new_timer()
-timer:start(300000, 0, vim.schedule_wrap(function()
+timer:start(5000, 0, vim.schedule_wrap(function()
 local handle = io.popen("cat /sys/class/power_supply/BAT1/capacity")
 local result = string.gsub(handle:read("*a"),"\n"," ")
 handle:close()
